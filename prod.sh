7d730d498b42aee99b78cb27a274bad59ac309f4
Ross Wintle <rosswintle@users.noreply.github.com>
2026-01-17 15:21:20 +0000

    Initial commit

---
+++ prod.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Add this if you want to build JavaScript with ESBuild - you will need to "npm install -D esbuild"
+# node_modules/.bin/esbuild src/js/turbo-admin/main.js --bundle --minify --sourcemap --outfile=public/js/turbo-admin/main.min.js
+
+# Build the site using the PHP builder script using the production environment
+php ./build.php --prod
+
+# Build Tailwind CSS styles for the site
+npm run build:prod
+
+# Cache busting for CSS
+NOW=`date +%s`
+rm public/css/styles-*.css
+cp public/css/styles.css public/css/styles-$NOW.css
+# Notes
+# 1. The -i flag requires a parameter on MacOS
+# 2. This only works on top-level files in public - not in subdirectories
+sed -i '' "s#css/styles.css#css/styles\-$NOW\.css#g" public/*.html
