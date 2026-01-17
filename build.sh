5cfa315dfcff953e5a09612348befa4fb94ca50f
Ross Wintle <rosswintle@users.noreply.github.com>
2026-01-17 20:14:21 +0000

    chore: Add echos to build scripts

--- build.sh
+++ build.sh
@@ -4,7 +4,12 @@
 # node_modules/.bin/esbuild src/js/main.js --bundle --minify --sourcemap --outfile=public/js/main.min.js
 
 # Build the site using the PHP builder script
+echo "Building HTML..."
 php ./build.php
 
 # Build Tailwind CSS styles for the site
+echo "Building Tailwind CSS"
 npm run build:dev
+
+echo "Done!"
+echo "Don't forget to use prod.sh if you're building for a commit/deploy!"
--- prod.sh
+++ prod.sh
@@ -4,12 +4,15 @@
 # node_modules/.bin/esbuild src/js/turbo-admin/main.js --bundle --minify --sourcemap --outfile=public/js/turbo-admin/main.min.js
 
 # Build the site using the PHP builder script using the production environment
+echo "Building HTML..."
 php ./build.php --prod
 
 # Build Tailwind CSS styles for the site
+echo "Building Tailwind CSS"
 npm run build:prod
 
 # Cache busting for CSS
+echo "Cache-busting files"
 NOW=`date +%s`
 rm public/css/styles-*.css
 cp public/css/styles.css public/css/styles-$NOW.css
@@ -17,3 +20,5 @@ cp public/css/styles.css public/css/styles-$NOW.css
 # 1. The -i flag requires a parameter on MacOS
 # 2. This only works on top-level files in public - not in subdirectories
 sed -i '' "s#css/styles.css#css/styles\-$NOW\.css#g" public/*.html
+
+echo "Done!"
